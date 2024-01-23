09c6e1e8ebd2c16cb3985c0bf7c5a532e5315b6e
jerry <krikeykrypto@gmail.com>
2024-01-22 23:24:35 -0600

    looks good

--- src/components/OpcatAndOrdinals/OpcatAndOrdinals.js
+++ src/components/OpcatAndOrdinals/OpcatAndOrdinals.js
@@ -26,10 +26,8 @@ const OpcatAndOrdinals = ({ onComplete }) => {
 
   const steps = [
     "An Overview of Ordinals",
-    "Asset Hashing",
-    "Using OP_CAT",
-    "Transaction Creation",
-    "Inscription and Validation",
+    "Theoretical Usage",
+    "Challenges & Considerations",
   ];
 
   return (
@@ -104,6 +102,33 @@ const OpcatAndOrdinals = ({ onComplete }) => {
               <Typography variant="h5" mt={3} mb={2} color="#FF10F0">
                 {steps[1]}
               </Typography>
+              <Typography paragraph>
+                Imagine that we have a typical set of layers for an NFT project
+                (i.e. Hat, Hair, Glasses, Background, etc). <br /> In a Bitcoin
+                transaction, OP_CAT could be used to concatenate these metadata
+                string together to accompany the Ordinal:
+              </Typography>
+
+              <Typography
+                paragraph
+                bgcolor="#FF10F0"
+                color="black"
+                p={1}
+                borderRadius="6px"
+              >
+                <code>
+                  (Ordinal Number)(Metadata for Hat) OP_CAT <br />
+                  (Resulting String)(Metadata for Hair ) OP_CAT <br />
+                  (Resulting String)(Metadata for Glasses) OP_CAT ... <br />{" "}
+                  <br />
+                  (Final Metadata String) OP_CHECKSIG
+                  <br />
+                </code>
+              </Typography>
+              <Typography paragraph>
+                The final metadata string represents all attributes concatenated
+                together and is associated with the ordinal number of a satoshi.
+              </Typography>
             </>
           )}
           {activeStep === 2 && (
@@ -111,20 +136,28 @@ const OpcatAndOrdinals = ({ onComplete }) => {
               <Typography variant="h5" mt={3} mb={2} color="#FF10F0">
                 {steps[2]}
               </Typography>
-            </>
-          )}
-          {activeStep === 3 && (
-            <>
-              <Typography variant="h5" mt={3} mb={2} color="#FF10F0">
-                {steps[3]}
-              </Typography>
-            </>
-          )}
-          {activeStep === 4 && (
-            <>
-              <Typography variant="h5" mt={3} mb={2} color="#FF10F0">
-                {steps[4]}
-              </Typography>
+              <ul style={{ listStyleType: "none", padding: 0 }}>
+                <li style={{ marginBottom: "10px" }}>
+                  <b>Data Size:</b> Bitcoin transactions are limited in size,
+                  and including extensive metadata directly in the transaction
+                  could quickly become impractical due to space constraints.
+                </li>
+                <li style={{ marginBottom: "10px" }}>
+                  <b>Cost:</b> The more data included in a transaction, the
+                  larger its size, and thus, the higher the fee to include it in
+                  the blockchain.
+                </li>
+                <li style={{ marginBottom: "10px" }}>
+                  <b>Flexibility:</b> Bitcoin's scripting language is not as
+                  flexible as Ethereum's, which supports Turing-complete smart
+                  contracts that can encode complex logic and data structures.
+                </li>
+                <li style={{ marginBottom: "10px" }}>
+                  <b>Upgrades:</b> Implementing such functionality would require
+                  changes to the Bitcoin protocol, which is difficult to achieve
+                  due to the need for network-wide consensus.
+                </li>
+              </ul>
             </>
           )}
 
